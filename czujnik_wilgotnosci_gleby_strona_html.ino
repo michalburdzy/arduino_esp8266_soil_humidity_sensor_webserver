9f2be7ee5b298e77f492acb2fbd0961e42d02902
Michal Burdzy <michal.burdzy@netguru.com>
2021-02-21 17:10:55 +0100

    working web server

--- czujnik_wilgotnosci_gleby_strona_html.ino
+++ czujnik_wilgotnosci_gleby_strona_html.ino
@@ -5,10 +5,23 @@
 #include <Wire.h>
 #include "AsyncJson.h"
 #include "ArduinoJson.h"
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-// Set your Static IP address
+const long utcOffsetInSeconds = 3600;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+#define SensorPin A0
+int sensorValue = 0;
+int calibrationLow = 2;
+int calibrationHigh = 1024;
+long lastMeasureTime = 0;
+long measureTimeDelay = 1000 * 10;
+
 IPAddress local_IP(192, 168, 0, 100);
-// Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1);
 
 IPAddress subnet(255, 255, 0, 0);
@@ -20,10 +33,7 @@ const char* password = "8aHEtkWAHx7SGZUDf4";
 
 // Set LED GPIO
 const int ledPin = 2;
-// Stores LED state
 String ledState;
-
-// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
 // Replaces placeholder with LED state value
@@ -46,6 +56,9 @@ void setup() {
   Serial.begin(115200);
   delay(200);
   pinMode(ledPin, OUTPUT);
+  pinMode(SensorPin, INPUT);
+
+  digitalWrite(ledPin, HIGH);
 
   // Initialize SPIFFS
   if (!SPIFFS.begin()) {
@@ -57,6 +70,7 @@ void setup() {
   }
 
   // Connect to Wi-Fi network with SSID and password
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -91,27 +105,78 @@ void setup() {
     request->send(SPIFFS, "/chartist.min.js", "text/javascript");
   });
 
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest * request) {
-    digitalWrite(ledPin, LOW);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+  server.on("/calibrate_min", HTTP_GET, [](AsyncWebServerRequest * request) {
+    calibrationLow = measureHumidity();
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    request->send(response);
+  });
+  server.on("/calibrate_max", HTTP_GET, [](AsyncWebServerRequest * request) {
+    calibrationHigh = measureHumidity();
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    request->send(response);
   });
 
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/led_on", HTTP_POST, [](AsyncWebServerRequest * request) {
+    digitalWrite(ledPin, LOW);
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    request->send(response);
+  });
+
+  server.on("/led_off", HTTP_POST, [](AsyncWebServerRequest * request) {
     digitalWrite(ledPin, HIGH);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    request->send(response);
+  });
+
+  server.on("/reset_data", HTTP_POST, [](AsyncWebServerRequest * request) {
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    File soilHumidityDataFile = SPIFFS.open("/data/humidity_data.csv", "w");
+    if (!soilHumidityDataFile) {
+      Serial.println("Error opening file 'humidity_data.csv'");
+      request->send(500);
+    }
+    soilHumidityDataFile.close();
+    lastMeasureTime = measureTimeDelay;
+    request->send(response);
   });
 
   server.on("/measure_humidity", HTTP_POST, [](AsyncWebServerRequest * request) {
+    int humidity = measureHumidity();
+
     AsyncResponseStream *response = request->beginResponseStream("application/json");
     DynamicJsonBuffer jsonBuffer;
     JsonObject &root = jsonBuffer.createObject();
-    root["soilHumidityPercent"] = 54;
+    root["soilHumidity"] = humidity;
     root.printTo(*response);
     request->send(response);
   });
 
+  server.on("/humidity_data", HTTP_GET, [](AsyncWebServerRequest * request) {
+    AsyncResponseStream *response = request->beginResponseStream("text/plain");
+    bool fileExists = SPIFFS.exists("/data/humidity_data.csv");
+    if (!fileExists) {
+      Serial.println("File '/data/humidity_data.csv' does not exist.Creating");
+      SPIFFS.open("/data/humidity_data.csv", "w").close();
+    }
+    File soilHumidityDataFile = SPIFFS.open("/data/humidity_data.csv", "r");
+    if (!soilHumidityDataFile) {
+      Serial.println("Error opening file 'humidity_data.csv'");
+      request->send(500);
+    } else {
+      String fileData;
+      while (soilHumidityDataFile.available()) {
+        fileData = soilHumidityDataFile.readString();
+      }
+
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject &root = jsonBuffer.createObject();
+      root["data"] = fileData;
+      root.printTo(*response);
+      request->send(response);
+    }
+
+    soilHumidityDataFile.close();
+  });
   //  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
   //    request->send_P(200, "text/plain", getTemperature().c_str());
   //  });
@@ -122,5 +187,45 @@ void setup() {
 }
 
 void loop() {
+  bool shouldMeasure = millis() > measureTimeDelay + lastMeasureTime;
 
+  if (shouldMeasure) {
+    String time = getCurrentTime();
+    lastMeasureTime = millis();
+    int humidity = measureHumidity();
+    File humidityWriteFile = SPIFFS.open("/data/humidity_data.csv", "a+");
+    if (!humidityWriteFile) {
+      Serial.println("Error opening file 'humidity_data.csv'");
+    }
+
+    humidityWriteFile.print(time);
+    humidityWriteFile.print(",");
+    humidityWriteFile.print(humidity);
+    humidityWriteFile.print("\n");
+    humidityWriteFile.close();
+  }
+
+}
+
+String getCurrentTime() {
+  timeClient.update();
+  return String(timeClient.getEpochTime() * 1000);
+}
+int measureHumidity() {
+  int result;
+  int measureLoops = 10;
+  int resultSum = 0;
+  for (int i = 0; i < measureLoops; i++) {
+    int value = analogRead(SensorPin);
+    int mappedValue = map(value, calibrationLow, calibrationHigh, 0, 100);
+    Serial.print("mapped value: ");
+    Serial.println(mappedValue);
+    resultSum += mappedValue;
+  }
+
+  result = resultSum / measureLoops;
+  Serial.print("returned value: ");
+  Serial.println(result);
+
+  return result;
 }
--- data/index.js
+++ data/index.js
@@ -1,12 +1,79 @@
-const button = document.getElementById('btn-measure-humidity')
+const fetchSavedData = async () => {
+  const  { data } = await fetch('/humidity_data').then(r => r.json()).catch(error => console.error(error))
+  const dataArray =data
+  .replace('↵', '\n')
+  .replace('\r', '\n')
+  .split('\n')
+  .map(el => ({
+    time: new Date(Number(el.split(',')[0])), 
+    soilHumidity:Number( el.split(',')[1])
+  }))
+  .filter(el => !!el.time && (!!el.soilHumidity || el.soilHumidity === 0))
+  console.log(dataArray);
+  humidityChart.update(parseHumidity(dataArray))
+}
+setTimeout(fetchSavedData, 500);
 
-button.addEventListener('click', async _ => {
+const humiditySensorRead = []
+const parseHumidity = (humiditySensorReadArray) => ({
+  series:[humiditySensorReadArray.map(el => el.soilHumidity)],
+  labels: [humiditySensorReadArray.map(el => el.time.toLocaleTimeString())]
+})
+const humidityChart = new Chartist.Line('#humidityChart', parseHumidity(humiditySensorRead), {
+  high: 100,
+  low: 0,
+  height: '200px',
+  // axisX: {
+  //   labelInterpolationFnc: function(value) {
+  //       const arr = value.split(":")
+  //       return  `${arr[0]}:${arr[1]}`
+  //   }
+  // },
+},
+  // ['screen and (max-width: 640px)', {
+  //   axisX: {
+  //     labelInterpolationFnc: function(value) {
+  //       const arr = (value[0] || '').split(':')
+  //       return `${arr[0]}:${arr[1]}`
+  //     }
+  //   }
+  // }]
+// ]
+)
+
+const updateSoilHumidityData = async () => {
   try {
     const response = await fetch('/measure_humidity', {
       method: 'post',
-      body: {
-        // Your body
-      }
+      body: {}
+    }).then(r => r.json())
+
+    const newValue = response.soilHumidity
+    document.querySelector('#current_soil_humidity').innerHTML = `Current soil humidity: ${newValue}`
+
+  } catch (err) {
+    console.error(`Error: ${err}`)
+  }
+}
+
+document.getElementById('btn-measure-humidity').addEventListener('click',updateSoilHumidityData)
+
+document.getElementById('led_on').addEventListener('click', async _ => {
+  try {
+   await fetch('/led_on', {
+      method: 'post',
+      body: {}
+    })
+  } catch (err) {
+    console.error(`Error: ${err}`)
+  }
+})
+
+document.getElementById('led_off').addEventListener('click', async _ => {
+  try {
+   await fetch('/led_off', {
+      method: 'post',
+      body: {}
     })
     console.log('Completed!', response.json())
   } catch (err) {
@@ -14,7 +81,29 @@ button.addEventListener('click', async _ => {
   }
 })
 
-new Chartist.Line('#chart1', {
-  labels: [1, 2, 3, 4, 6, 7],
-  series: [[4, 15, 34, 65, 69, 59, 61]]
+document.getElementById('reset_data').addEventListener('click', async _ => {
+  try {
+    const proceed = (prompt("This will remove ALL saved data. Type 'ok' to proceed") || '').toLowerCase()
+    if(proceed === 'ok'){
+      await fetch('/reset_data', {
+        method: 'post',
+      })
+      humidityChart.update(parseHumidity([]))
+      setTimeout(fetchSavedData, 500)
+    }
+  } catch (err) {
+    console.error(`Error: ${err}`)
+  }
 })
+
+document.getElementById('update_data').addEventListener('click', async _ => {
+  fetchSavedData()
+})
+document.getElementById('calibrate_min').addEventListener('click', async _ => {
+ await fetch('/calibrate_min')
+})
+document.getElementById('calibrate_max').addEventListener('click', async _ => {
+ await fetch('/calibrate_max')
+})
+
+setInterval(fetchSavedData, 1000 * 60 * 10) // update every 10 minutes
